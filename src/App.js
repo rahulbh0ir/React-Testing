850cbd1bf67213cde55c85c932097d93577cc27c
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-02-03 11:24:19 +0530

    up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import { RouterProvider } from "react-router-dom";
-import Navbar from "./pages/Navbar";
-import { createBrowserRouter} from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Blogs from './pages/Blogs';
 
-function App() {
-  const router = createBrowserRouter([
-      {path: "/", element: <><Navbar/> <Home /> </> },
-      {path: "/about", element :<> <Navbar/> <About /> </>},
-      {path: "/blogs", element: <><Navbar/> <Blogs /> </> }
-  ])
+function App({router}) {
+  
   return (
     <>
     <RouterProvider router={router}/>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Navbar from "./pages/Navbar";
+import { createBrowserRouter} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blogs from './pages/Blogs';
 
-
-
-
+const router = createBrowserRouter([
+  {path: "/", element: <><Navbar /><Home /> </>},
+  {path: "/a" , element: <> <Navbar /> <About /> </>},
+  {path: "/b" , element: <> <Navbar/> <Blogs /> </>}
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
-      <App/>
-     </>
+      <App router = {router} />
+    </>
 );
 
