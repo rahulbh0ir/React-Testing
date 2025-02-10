4423296331c8810e3df775e6e571b3240c8e604d
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-02-10 16:48:51 +0530

    up

--- src/pages/Fetch.js
+++ src/pages/Fetch.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const Fetch = () => {
+  const [loading, setLoading] = useState(true);
+  const [post, setPost] = useState({});
+  const [err, setErr] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then(res => {
+        console.log(res)
+        res.json()
+        console.log(res) 
+      })
+      .catch(err => {
+        console.log(err)
+      })  
+  }, [])   
+
   return (
-    <div>
-      fetch
-    </div>
+    <>
+    <h3>Fetch</h3>
+   
+    </>
   )
 }
 
