f47d5c7276205a6d9f2ff7b1141f86a8559dcbd0
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-02-19 16:26:42 +0530

    up

--- src/pages/ReducerHook.js
+++ src/pages/ReducerHook.js
@@ -5,7 +5,8 @@ const ReducerHook = () => {
   const red = useContext(context)  
   return (
     <>
-    <h2>{red.count}</h2>
+    <h2>Gobal State Example with UseReducer</h2>
+    <h2 style={{color: "red"}}>{red.count}</h2>
     <button onClick={() => red.func("plus")}>Plus</button>  
     <button onClick={() => red.func("minus")}>Minus</button>  
     <button onClick={() => red.func("reset")}>Reset</button>  
